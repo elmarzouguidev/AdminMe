8bb40811b1b337e3a8e44e39a4c3074052498d20
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-05-28 18:49:05 +0100

    add Post Model with migration

--- app/Models/CMS/Post.php
+++ app/Models/CMS/Post.php
@@ -2,10 +2,12 @@
 
 namespace App\Models\CMS;
 
+use App\Traits\GetModelByKeyName;
+use App\Traits\UuidGenerator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Category extends Model
+class Post extends Model
 {
     use HasFactory;
     use UuidGenerator;
---
+++ database/migrations/2024_05_28_174451_create_posts_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use App\Models\CMS\Category;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->uuid()->nullable();
+
+            $table->string('title');
+            $table->string('slug');
+            $table->longText('content')->nullable();
+            $table->mediumText('excerpt')->nullable();
+            $table->json('options')->nullable();
+
+            $table->foreignIdFor(Category::class)->index()->nullable()->constrained()->nullOnDelete();
+
+            $table->foreignIdFor(User::class)->index()->nullable()->constrained()->nullOnDelete();
+
+            $table->boolean('active')->default(false);
+            $table->dateTime('published_at')->nullable();
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
