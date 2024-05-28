ec1b1ab243eef0eb4664ef56d7b040e1c7fb1b08
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-05-28 18:57:22 +0100

    add spatie/media to the application

--- app/Models/CMS/Category.php
+++ app/Models/CMS/Category.php
@@ -1,7 +1,8 @@
 <?php
 
 namespace App\Models\CMS;
-
+use App\Traits\GetModelByKeyName;
+use App\Traits\UuidGenerator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
--- app/Models/CMS/Post.php
+++ app/Models/CMS/Post.php
@@ -6,10 +6,13 @@ use App\Traits\GetModelByKeyName;
 use App\Traits\UuidGenerator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Post extends Model
+class Post extends Model implements HasMedia
 {
     use HasFactory;
     use UuidGenerator;
     use GetModelByKeyName;
+    use InteractsWithMedia;
 }
