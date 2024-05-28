4bef1294a429fa387205f079b6a78279005111e8
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-05-28 18:37:34 +0100

    change default connection from Sqlite to Mysql

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('telephone', 30)->nullable()->unique();
+            $table->string('username', 40)->nullable()->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
