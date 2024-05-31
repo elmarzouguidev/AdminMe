a870b0bab29672628f72fb7c8ecbebde6ac35b3d
elmarzouguidev <elmarzouguiabdelghafour@gmail.com>
2024-05-31 12:24:46 +0100

    add Resend mailer to the application v-2

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,24 @@
 <?php
 
+use App\Mail\Resend\MailTest;
+use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
+use Resend\Laravel\Facades\Resend;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+
+Route::get('/resend', function () {
+    Mail::to('abdelgha4or@gmail.com')->send(new MailTest());
+
+    /*$mailerFrom = env('MAIL_FROM_ADDRESS');
+
+    Resend::emails()->send([
+        'from' => "Facturis <$mailerFrom>",
+        'to' => ['abdelgha4or@gmail.com'],
+        'subject' => 'hello world',
+        'html' => (new MailTest())->render(),
+    ]);*/
 });
